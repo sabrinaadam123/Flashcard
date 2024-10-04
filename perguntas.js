e9ae6685466568ee93c6ca89df536a6621b80455
Sabrina Adam <142607875+sabrinaadam123@users.noreply.github.com>
2024-10-04 09:23:31 -0300

    js

--- perguntas.js
+++ perguntas.js
@@ -1,7 +1,7 @@
 criaCartao(
     'Programação',
-   'O que é JavaScript?' , 
-    'O JavaScript é uma linguagem de programação'
+   'O que é Python?' , 
+    'O Python é uma linguagem de programação'
 )
 
 criaCartao(
